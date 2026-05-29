bbe343c30cf32528dc12ed3e9fa8d089018a71f8
[prthmesh thorat] <[thoratprathemesh@gmail.com]>
2026-05-30 05:22:52 +0530

    Navbar update

--- Email Summerization/src/components/Navbar/Navbar.jsx
+++ Email Summerization/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import './Navbar.css';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const handleLogout = async () => {
-  await fetch(`${import.meta.env.VITE_API_URL}/logout`, {
-    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-  });
-  localStorage.removeItem("token");
-  localStorage.removeItem("summaryRange");
-  navigate("/SignUp");
-};
+    await fetch(`${import.meta.env.VITE_API_URL}/logout`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+    });
+    localStorage.removeItem("token");
+    localStorage.removeItem("summaryRange");
+    navigate("/SignUp");
+  };
+
+  const isActive = (path) => location.pathname === path;
+
   return (
     <header className="navbar-header">
       <div className="navbar-container">
         <div className="navbar-left">
           <span className="navbar-logo">SummarizeAI</span>
           <nav className="navbar-links">
-            <a href="" className="nav-link active">Features</a>
-            <Link to="/TasksPage" className='nav-link'>Tasks</Link>
-            <Link to="/SummaryHistory" className='nav-link'>Summaries</Link>
+            <Link to="/" className={`nav-link ${isActive('/Dashboard') ? 'active' : ''}`}>Dashboard</Link>
+            <Link to="/TaskPage" className={`nav-link ${isActive('/TaskPage') ? 'active' : ''}`}>Tasks</Link>
+            <Link to="/SummaryHistory" className={`nav-link ${isActive('/SummaryHistory') ? 'active' : ''}`}>Summaries</Link>
           </nav>
         </div>
-        <button className="nav-btn"  onClick={() =>handleLogout()}>Log Out</button>
+        <button className="nav-btn" onClick={handleLogout}>Log Out</button>
       </div>
     </header>
   );
